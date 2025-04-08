8ed1a67aad4b7285f550dc90f74a3f53f1016dc0
Ot Roca <otrocadev@gmail.com>
2025-04-08 17:19:37 +0200

    Add landing page to be the place to see all the projects that will be added to the entire build

---
+++ landing/src/projects.js
@@ -0,0 +1,14 @@
+export const PROJECTS = [
+  {
+    slug: '01-tinder-swipe',
+    title: 'Tinder Swipe',
+    hidden: false,
+    description: 'A tinder swipe behaviour clone.',
+    learnings: [
+      'DOM',
+      'Animaciones con gestos',
+      'Eventos de mouse y touch',
+      'Transiciones CSS',
+    ],
+  },
+]
