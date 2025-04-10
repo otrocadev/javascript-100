ae028f609a5b7f208bcb45994ea080f37b5dec1d
Ot Roca <otrocadev@gmail.com>
2025-04-11 00:01:23 +0200

    Add Description component with overall process and update layout with macbook mockup for enhanced project presentation

--- landing/src/projects.js
+++ landing/src/projects.js
@@ -2,14 +2,14 @@ export const PROJECTS = [
   {
     slug: '01-tinder-swipe',
     title: 'Tinder Swipe',
-    hidden: false,
+    laptop: false,
     description: 'A tinder swipe behaviour clone.',
     learnings: ['DOM', 'Gesture animations', 'Mouse and touch events'],
   },
   {
     slug: '02-moto-scroll',
     title: 'Moto Scroll',
-    hidden: false,
+    laptop: true,
     description:
       'An animation that spins the Royal Enfield HNTR 350 with the scroll position.',
     learnings: ['DOM', 'Animation on Scroll', 'Frames handling'],
