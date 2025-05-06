3c2971db14642cda99044c1cbf6c4af0f4a039b5
Ot Roca <otrocadev@gmail.com>
2025-05-06 19:33:22 +0200

    Refactor HTML structure and styles for MM93 championship page; add racer images and update project descriptions

--- landing/src/projects.js
+++ landing/src/projects.js
@@ -3,7 +3,8 @@ export const PROJECTS = [
     slug: '01-tinder-swipe',
     title: 'Tinder Swipe',
     laptop: false,
-    description: 'A tinder swipe behaviour clone.',
+    description:
+      'A tinder swipe clone that emulates the behaviour of the world famous dating app Tinder.',
     learnings: ['DOM', 'Gesture animations', 'Mouse and touch events'],
   },
   {
@@ -11,7 +12,15 @@ export const PROJECTS = [
     title: 'Moto Scroll',
     laptop: true,
     description:
-      'An animation that spins the Royal Enfield HNTR 350 with the scroll position.',
+      'A simplte, yet amazing animation that spins a motorbike (Royal Enfield HNTR 350) depending on the current scroll position.',
     learnings: ['DOM', 'Animation on Scroll', 'Frames handling'],
+  },
+  {
+    slug: '03-mm93-points-to-championship',
+    title: 'MM93 points to 2025 MotoGP title',
+    laptop: true,
+    description:
+      'This landing shows the points that Marc Marquez needs to score ion order to be oifficially the 2025 MotoGP World Champion.',
+    learnings: ['Basic math operations', 'Data fetching'],
   },
 ]
